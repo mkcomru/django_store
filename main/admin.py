b0af8adc1f18a92b3f20b2a9f318c588a34593ad
mkcomru <grebenshchikov.my@students.dvfu.ru>
2025-06-02 22:16:35 +1000

    finish project

--- main/admin.py
+++ main/admin.py
@@ -1,39 +1,37 @@
 from django.contrib import admin
-from main.models import Size, ClothingItemSize, \
-    ClothingItem, Category
+from .models import Size, Category, \
+    ClothingItem, ClothingItemSize, ItemImage
 
 
-class ClothingItemSizeInline(admin.StackedInline):
+class ClothingItemSizeInline(admin.TabularInline):
     model = ClothingItemSize
+    extra = 4
+
+
+class ItemImageInline(admin.TabularInline):
+    model = ItemImage
     extra = 4
 
 
 @admin.register(Size)
 class SizeAdmin(admin.ModelAdmin):
-    list_display = ("name",)
-    search_fields = ("name",)
+    list_display = ('name',)
+    search_fields = ('name',)
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("name", "slug")
-    prepopulated_fields = {"slug": ("name",)}
-    search_fields = ("name",)
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
 
 
 @admin.register(ClothingItem)
 class ClothingItemAdmin(admin.ModelAdmin):
-    list_display = ("name", "slug", "available", "price",
-                    "discount", "created_at", "updated_at")
-    readonly_fields = ("created_at", "updated_at")
-    list_filter = ("available", "category")
-    prepopulated_fields = {"slug": ("name",)}
-    ordering = ("-created_at",)
-    search_fields = ("name",)
-    inlines = [ClothingItemSizeInline]
-
-
-
-
-
-
+    list_display = ('name', 'slug', 'category',
+                    'available', 'price', 'discount',
+                    'created_at', 'updated_at')
+    list_filter = ('available', 'category')
+    prepopulated_fields = {'slug': ('name',)}
+    ordering = ('-created_at',)
+    inlines = [ClothingItemSizeInline, ItemImageInline]
--- main/models.py
+++ main/models.py
@@ -1,72 +1,76 @@
-from django.db import models
+from django.db import models 
 
 
 class Size(models.Model):
     name = models.CharField(max_length=10, unique=True)
-    objects = models.Manager()
+
 
     def __str__(self):
         return self.name
-
-    class Meta:
-        verbose_name="Size"
-        verbose_name_plural="Sizes"
-
+    
 
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
     slug = models.SlugField(unique=True)
-    objects = models.Manager()
+
 
     def __str__(self):
         return self.name
+    
 
     class Meta:
-        verbose_name="Category"
-        verbose_name_plural="Categories"
-        ordering = ("name",)
-        indexes = [models.Index(fields=["name"])]
+        ordering = ['name']
+        indexes = [models.Index(fields=['name'])]
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    
+    def get_item_count(self):
+        return ClothingItem.objects.filter(category=self).count()
 
 
 class ClothingItem(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
     slug = models.SlugField(unique=True)
     available = models.BooleanField(default=True)
-    price = models.DecimalField(max_digits=20, decimal_places=2)
-    discount = models.DecimalField(max_digits=5, decimal_places=2)
-    sizes = models.ManyToManyField(Size, through="ClothingItemSize",
-                                    related_name='clothing_item', blank=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE,
-                                related_name='clothing_items')
+    sizes = models.ManyToManyField(Size, through='ClothingItemSize',
+                                   related_name='clothing_item', blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, 
+                                 related_name='clothing_items')
+    image = models.ImageField(upload_to='product/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True)
-    objects = models.Manager()
+    price = models.DecimalField(max_digits=20, decimal_places=2)
+    discount = models.DecimalField(max_digits=5, decimal_places=2)
+    
 
     def __str__(self):
         return self.name
+    
 
     def get_price_with_discount(self):
-        price = (self.price * (1 - self.discount / 100)) if self.discount else self.price
-        return price
-
-    class Meta:
-        verbose_name="ClothingItem"
-        verbose_name_plural="ClothingItems"
-
+        if self.discount > 0:
+            return round(self.price * (1 - (self.discount / 100)), 2)
+        return round(self.price, 2)
+    
 
 class ClothingItemSize(models.Model):
     clothing_item = models.ForeignKey(ClothingItem, on_delete=models.CASCADE)
     size = models.ForeignKey(Size, on_delete=models.CASCADE)
     available = models.BooleanField(default=True)
-    objects = models.Manager()
 
-    def __str__(self):
-        return self.clothing_item.name
 
     class Meta:
-        unique_together = ("clothing_item", "size")
-        verbose_name="ClothingItemSize"
-        verbose_name_plural="ClothingItemSizes"
+        unique_together = ('clothing_item', 'size')
 
+    
+class ItemImage(models.Model):
+    product = models.ForeignKey(ClothingItem, related_name='images',
+                                on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product/%Y/%m/%d', blank=True)
 
+    
+    def __str__(self):
+        return f'{self.product.name} - {self.image.name}'
+    
